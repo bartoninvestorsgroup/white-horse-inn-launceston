0a460f99da772203b5c90e26c29624fb853ec095
TweetingCynical <jon.garnettsmith@gmail.com>
2026-06-23 18:48:58 +0100

    Added Facebook redirect

--- middleware.js
+++ middleware.js
@@ -1,6 +1,27 @@
 import { NextResponse } from "next/server";
+import siteSettings from "./src/data/site-settings.json";
+
+function normalizeExternalUrl(value) {
+  if (!value) {
+    return "";
+  }
+
+  return /^https?:\/\//i.test(value) ? value : `https://${value}`;
+}
 
 export function middleware(request) {
+  if (request.nextUrl.pathname === "/facebook") {
+    const facebookUrl = normalizeExternalUrl(siteSettings.socialLinks?.facebook);
+
+    try {
+      if (facebookUrl) {
+        return NextResponse.redirect(new URL(facebookUrl), 307);
+      }
+    } catch {
+      // Fall through to the site if the configured URL is not valid.
+    }
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-pathname", request.nextUrl.pathname);
 
